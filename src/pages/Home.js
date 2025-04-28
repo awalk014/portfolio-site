22bf7ef70579620d15ee12c39e5f21f9ef627b15
Benjamin Walker <abramwalker2014@gmail.com>
2025-04-28 13:40:08 -0400

    push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,11 @@ class Home extends React.Component {
     render() {
         return (
             <main>
-            <h1>Welcome</h1>
-            <p>This is the landing page for my educational portfolio.</p>
-            <p>Use the links above to navigate the site.</p>
-            <p>Please enjoy your stay.</p>
-        </main>
+                <h1>Welcome</h1>
+                <p>This is the landing page for my educational portfolio.</p>
+                <p>Use the links above to navigate the site.</p>
+                <p>Please enjoy your stay.</p>
+            </main>
         );
     }
 }
