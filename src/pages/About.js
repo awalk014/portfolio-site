e7dd70c2adeba4eee2f1eb78bf81c5b946c0126a
Benjamin Walker <abramwalker2014@gmail.com>
2025-05-05 18:05:57 -0400

    Nearly finished with this thing!

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,34 +5,45 @@ class About extends React.Component {
     render() {
         return (
             <main>
-            <div className="left_side">
-                <h1>Who Am I?</h1>
-                <p>My name is Benjamin Abram Walker. You can call me Abram.</p>
-                <p>I am a software development student graduating from Wayne Community College in May of 2025.
-                    My career has been in the field of industrial maintenance and manufacturing. I have also served
-                    in the United States Air Force Reserves as an aircraft mechanic for six years. I am a father 
-                    to two boys and a husband to a wonderful wife.
-                </p>
-                <h2>My Personal Philosophy</h2>
-                <p>
-                    Software development is a journey that has been similar to learning mechaincs. I see every system
-                    as being able to be broken down into its smallest components in order to be maintained and improved.
-                    I feel the same satisfaction writing code as I used to feel when I would replace valves and pumps
-                    on aircraft during my military career.
-                </p>
-                <h2>My Professional Goals</h2>
-                <p>
-                    My main aspiration is to one day work on backend systems in the software development field. I have
-                    always derived much satisfaction from working on systems that are not visible to the end user.
-                    There is a special pride in being a key player in the unseen systems that make the world go round.
-                </p>
-                <h3 className='clickable-link'><Link to="#">Resume</Link></h3>
-                <h3 className='clickable-link'><Link to="#">LinkedIn Profile</Link></h3>
-            </div>
-            <div className="right_side">
-                <img src="https://placehold.co/400x600" alt="Smiling handsome software developer" />
-            </div>
-        </main>
+                <div className="aboutme">
+                    <div className="left_side">
+                        <h1>Who Am I?</h1>
+                        <p>My name is Benjamin Abram Walker. You can call me Abram.</p>
+                        <p>I am a software development student graduating from Wayne Community College in May of 2025.
+                            My career has been in the field of industrial maintenance and manufacturing. I have also served
+                            in the United States Air Force Reserves as an aircraft mechanic for six years. I am a father
+                            to two boys and a husband to a wonderful wife.
+                        </p>
+                        <h2>My Personal Philosophy</h2>
+                        <p>
+                            Software development is a journey that has been similar to learning mechaincs. I see every system
+                            as being able to be broken down into its smallest components in order to be maintained and improved.
+                            I feel the same satisfaction writing code as I used to feel when I would replace valves and pumps
+                            on aircraft during my military career.
+                        </p>
+                        <h2>My Professional Goals</h2>
+                        <p>
+                            My main aspiration is to one day work on backend systems in the software development field. I have
+                            always derived much satisfaction from working on systems that are not visible to the end user.
+                            There is a special pride in being a key player in the unseen systems that make the world go round.
+                        </p>
+                        <h3 className='clickable-link'><Link to="#">Resume</Link></h3>
+                        <h3 className='clickable-link'><Link to="#">LinkedIn Profile</Link></h3>
+                    </div>
+                    <div className="right_side">
+                        <img src="https://placehold.co/400x600" alt="Smiling handsome software developer" />
+                    </div>
+                </div>
+                <div className='reflection'>
+                    <h1>Reflection</h1>
+                    <p>Upon completion of this website, I have new found appreciation of the role a front end or full stack developer.
+                        I have learned that the front end is not just a pretty face, but a complex system of components that work together to create a seamless user experience.
+                        There are many tools and libraries that can be used to create a website, and it is important to choose the right ones for the job.
+                        I have also learned that the front end is not just about the code, but also about the design and user experience.
+                        I have learned that a good front end developer must be able to think like a user and understand their needs and desires.
+                    </p>
+                </div>
+            </main>
         );
     }
 }
--- src/pages/CourseWork.js
+++ src/pages/CourseWork.js
@@ -22,31 +22,77 @@ function CourseWork() {
         <main>
             <h1>Please click on the links to review examples of my assignments below.</h1>
             <div className='div_coursework'>
-                <h3>CSC 151 - Mobile Application Development I</h3>
+                <h3>WEB 151 - Mobile Application Development I</h3>
                 {/* Make the title clickable */}
                 <p>
                     <span
                         className="clickable-link"
-                        onClick={() => openModal('video', '/videos/web151appvid.mp4', 'CSC 151 - Final Mobile Application Demo')}
+                        onClick={() => openModal('video', '/videos/web151appvid.mp4', 'WEB 151 - Final Mobile Application Demo')}
                     >
                         Final Mobile Application
                     </span>
                     {' | '} {/* Separator */}
                     <span
                         className="clickable-link"
-                        onClick={() => openModal('image', '/images/web151blocks.png', 'CSC 151 - Application Blocks')}
+                        onClick={() => openModal('image', '/images/web151blocks.png', 'WEB 151 - Application Blocks')}
                     >View Blocks</span>
                     {' | '} {/* Separator */}
                     <span
                         className="clickable-link"
-                        onClick={() => openModal('image', '/images/web151designer.png', 'CSC 151 - Application Designer')}
+                        onClick={() => openModal('image', '/images/web151designer.png', 'WEB 151 - Application Designer')}
                     >View Designer</span>
                 </p>
-                <p>This is the app I used for my final project in my Android app development class. I made a space invaders inspired game using the pictures and sounds of one my favorite irreverent cartoons from my childhood. It was a challenging process to create the game logic with basic tools not geared towards that kind of development. I learned how to use the images as objects and how to manipulate their state with timers and interactivity by checking for if the positions of the objects intersected.</p>
+                <p>This is the app I used for my final project in my Android app development class. I made a space invaders inspired game using the pictures and sounds of one my favorite irreverent cartoons from my childhood.
+                     It was a challenging process to create the game logic with basic tools not geared towards that kind of development. I learned how to use the images as objects and how to manipulate their state with timers and interactivity by checking for if the positions of the objects intersected.</p>
             </div>
+
+            <div className='div_coursework'>
+                <h3>CSC 151 - Java Programming</h3>
+                <a
+                    href="https://github.com/awalk014/SchoolWork/tree/cc2e06f0e6e13b81c093b24b1c1c446ab67eba9f/Final"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="clickable-link"
+                >Java Programming Final Project</a>
+                <p>This is the link to my final project for my Java Programming class. We had to build a program that simulates the software a car dealership would use to store all relevant information
+                    about the cars they have for sale. I used Java's object-oriented programming features to create a program that allows the user to add, remove, and search for cars in the dealership's inventory. I also used Java's file I/O capabilities to save and load the inventory from a file.
+                </p>
+            </div>
+
             <div className='div_coursework'>
                 <h3>CSC 251 - Advanced Java Programming</h3>
+                <p>
+                <span
+                        className="clickable-link"
+                        onClick={() => openModal('video', '/videos/LatinTranslate_Encode.mp4', 'CSC 251 - Latin Translate Project Video')}
+                    >Latin Translate Project</span>
+                {' | '} {/* Separator */}
+                <a
+                    href="https://github.com/awalk014/SchoolWork/tree/557d32cd77ac1dac333a2f39e7733852f67e3c20/LatinTranslator_BenjaminWalker"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="clickable-link"
+                >Latin Translate Project (NetBeans Project)</a>
+                </p>
+                <p>This project demonstrates the basics of using GUI elements with JavaFX. Learning how to use the fundamental objects (Stages, Scenes, Nodes, etc.) is essential for building 
+                    applications with human-friendly interfaces. Customer facing software is only as good as its user experience.</p>
+            </div>
 
+            <div className='div_coursework'>
+                <h3>WEB 182 - PHP Programming</h3>
+                <p>
+                <a
+                    href="https://github.com/awalk014/SchoolWork/blob/8975d51b36ecd2585e8789e9f436d2ae5698a84b/send-email.php"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="clickable-link"
+                >PHP Send Email Script</a>
+                </p>
+                <p>I have included an anonmyous version of my PHP script hosted on my backend site. I have not included any personalable identifiable information for security purposes.
+                    This script is a simple example of how to send an email using PHP. It uses the PHPMailer library to send the email and includes basic error handling. 
+                    The script takes the recipient's email address, subject, and message as input and sends the email using the SMTP protocol. 
+                    This is a common task in web development, and I have used this script in several projects to send notifications and alerts.
+                </p>
             </div>
 
 
